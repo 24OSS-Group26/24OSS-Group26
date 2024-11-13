cf964318ac39600fba4e9c18ad8c524561bfca21
songyb111-gachon <songyb111@gachon.ac.kr>
2024-11-13 15:50:49 +0900

    gif 생성 시 파일명에 날짜시간 정보 붙게 수정

--- README_img/gif/create_gif.py
+++ README_img/gif/create_gif.py
@@ -1,28 +1,28 @@
 from PIL import Image
 import os
+from datetime import datetime
 
 
 def create_gif(image_folder, output_gif_path, duration=600):
-    # 현재 폴더에 있는 모든 .png 파일을 가져옵니다.
     image_files = [f"{i}.png" for i in range(1, 19)]  # 1.png부터 18.png까지 예시 (필요에 따라 범위 조정)
 
     images = []
     max_width = 0
     max_height = 0
 
-    # 첫 번째 이미지를 확인하여 최대 크기를 계산합니다.
+    # 첫 번째 이미지를 확인하여 최대 크기를 계산
     for image_file in image_files:
         image_path = os.path.join(image_folder, image_file)
         if os.path.exists(image_path):
             image = Image.open(image_path)
-            # 가장 큰 이미지의 크기를 구합니다.
+            # 가장 큰 이미지의 크기
             max_width = max(max_width, image.width)
             max_height = max(max_height, image.height)
         else:
             print(f"파일이 존재하지 않습니다: {image_path}")
             return
 
-    # 각 이미지를 열고 크기를 맞추고, 작은 이미지는 중앙에 배치합니다.
+    # 각 이미지를 열고 크기를 맞추고, 작은 이미지는 중앙에 배치
     for image_file in image_files:
         image_path = os.path.join(image_folder, image_file)
         if os.path.exists(image_path):
@@ -46,10 +46,11 @@ def create_gif(image_folder, output_gif_path, duration=600):
 
 # 사용 예시
 if __name__ == "__main__":
-    # 현재 스크립트가 위치한 폴더 경로를 구합니다.
+    # 현재 스크립트가 위치한 폴더 경로
     current_folder = os.path.dirname(os.path.realpath(__file__))
 
-    # GIF 파일을 생성할 경로
-    output_gif_path = os.path.join(current_folder, "output.gif")  # 동일 폴더에 저장될 output.gif
+    # 현재 날짜와 시간을 추가한 파일 이름 생성
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")  # "YYYYMMDD_HHMMSS" 형식
+    output_gif_path = os.path.join(current_folder, f"output_{timestamp}.gif")
 
     create_gif(current_folder, output_gif_path)
